6d888f2a6eb1e7c509234f509c1d28c5521cceff
yuriy <yuriyyakushev@yandex.ru>
2023-03-28 22:05:26 +0300

    KS-1-1 Two variables have been created

--- src/main/kotlin/Lesson_1/lesson_1_task_1.kt
+++
@@ -1,6 +0,0 @@
-package Lesson_1
-
-fun main() {
-
-    println("Это решение задачи 1 из урока 1")
-}
---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,6 @@
+package lesson_1
+
+fun main(){
+    var totalOrders = 0
+    val gratitude = "Thank you for ordering. I hope we have pleased you."
+}
