03737012bf096ff54f5659d8718586f5ec71c22d
yuriy <yuriyyakushev@yandex.ru>
2023-03-31 20:56:55 +0300

    KS-2-1 The code was formatted

--- src/main/kotlin/lesson_2/lesson2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson2_task_1.kt
@@ -1,7 +1,8 @@
 package lesson_2
+
 import java.util.Locale
 
 fun main() {
     val classGrades = arrayListOf(3, 4, 3, 5)
-    println(String.format(Locale.GERMANY,"%.2f",classGrades.average()))
+    println(String.format(Locale.GERMANY, "%.2f", classGrades.average()))
 }
