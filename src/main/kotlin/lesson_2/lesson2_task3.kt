903078924a3ff11001068dc69124fdc12ce474fd
yuriy <yuriyyakushev@yandex.ru>
2023-03-31 21:19:34 +0300

    KS-2-3 Rewrote the solution

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,18 +1,11 @@
 package lesson_2
 
-fun main(){
-    val departureHour = 9
+fun main() {
+    val departureHours = 9
     val departureMinutes = 39
     val travelTime = 457
+    val timeInMinutes = (departureHours * 60 + departureMinutes + travelTime)
 
-    fun timeSecondsToStr(minutes: Int): String {
-        val hours = minutes / 60 + departureHour
-        val remМinutes = minutes % 60 + departureMinutes
-        val totalHours = (remМinutes / 60) + hours
-        val totalMinutes = (remМinutes % 60)
-        return String.format("%02d:%02d",  totalHours, totalMinutes)
-    }
-
-    println(timeSecondsToStr(travelTime))
+    println(String.format("%02d:%02d", (timeInMinutes / 60) % 24, timeInMinutes % 60))
 
 }
