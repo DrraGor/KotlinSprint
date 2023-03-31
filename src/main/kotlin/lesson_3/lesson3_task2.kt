0c9cb8b3eaf03e9ab1b86c43212da2a92e3db401
yuriy <yuriyyakushev@yandex.ru>
2023-03-31 22:05:26 +0300

    KS-3-2 Solving task 2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    val maidenLastName = "Андреева"
+    val lastName = "Сидорова"
+    val patronymic = "Сергеевна"
+    val marriageAge = 22
+    val age = 20
+    println("$maidenLastName $firstName $patronymic, $age")
+    println("$lastName $firstName $patronymic, $marriageAge")
+
+}
