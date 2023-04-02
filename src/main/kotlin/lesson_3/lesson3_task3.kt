9ff58586d63e15d1ecf99a76bbbe16e63935931f
yuriy <yuriyyakushev@yandex.ru>
2023-04-02 11:58:44 +0300

    KS-3-3 Rewrote the code as a single line

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -2,15 +2,8 @@ package lesson_3
 
 fun main() {
     val number = 3
-    println(    "$number х 1 = ${number * 1}\n" +
-                "$number х 2 = ${number * 2}\n" +
-                "$number х 3 = ${number * 3}\n" +
-                "$number х 4 = ${number * 4}\n" +
-                "$number х 5 = ${number * 5}\n" +
-                "$number х 6 = ${number * 6}\n" +
-                "$number х 7 = ${number * 7}\n" +
-                "$number х 8 = ${number * 8}\n" +
-                "$number х 9 = ${number * 9}"
+    println(
+        "$number х 1 = ${number * 1}\n$number х 2 = ${number * 2}\n$number х 3 = ${number * 3}\n$number х 4 = ${number * 4}\n$number х 5 = ${number * 5}\n$number х 6 = ${number * 6}\n$number х 7 = ${number * 7}\n$number х 8 = ${number * 8}\n$number х 9 = ${number * 9}"
     )
 
 }
