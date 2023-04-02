da2569608b09df7d68aaf9ecb55be324ed55d8df
yuriy <yuriyyakushev@yandex.ru>
2023-04-02 17:03:06 +0300

    KS-4-2 Solving task number 1

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,11 @@
+package lesson_4
+
+fun main() {
+    val minAverageWeight = 35
+    val maxAverageWeight = 100
+    val maxAverageVolume = 100
+
+    println("Average для груза с весом 20 кг и объемом 80 л: ${20 >= minAverageWeight && 20 <= maxAverageWeight && 80 < maxAverageVolume}")
+    println("Average для груза с весом 50 кг и объемом 100 л: ${50 >= minAverageWeight && 50 <= maxAverageWeight && 100 < maxAverageVolume}")
+
+}
