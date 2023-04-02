a96752e3a65e8b45a7855890d4a9773e31e76757
yuriy <yuriyyakushev@yandex.ru>
2023-04-02 15:50:46 +0300

    KS-3-3 Solving task number 3 with """

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -3,7 +3,17 @@ package lesson_3
 fun main() {
     val number = 3
     println(
-        "$number х 1 = ${number * 1}\n$number х 2 = ${number * 2}\n$number х 3 = ${number * 3}\n$number х 4 = ${number * 4}\n$number х 5 = ${number * 5}\n$number х 6 = ${number * 6}\n$number х 7 = ${number * 7}\n$number х 8 = ${number * 8}\n$number х 9 = ${number * 9}"
+        """
+        $number х 1 = ${number * 1}
+        $number х 2 = ${number * 2}
+        $number х 3 = ${number * 3}
+        $number х 4 = ${number * 4}
+        $number х 5 = ${number * 5}
+        $number х 6 = ${number * 6}
+        $number х 7 = ${number * 7}
+        $number х 8 = ${number * 8}
+        $number х 9 = ${number * 9}
+           """.trimIndent()
     )
 
 }
