9e882ad5e0882124468098a27e12e5723c326cf0
yuriy <yuriyyakushev@yandex.ru>
2023-04-03 21:04:29 +0300

    KS-4-2 Solving task number 2 without magic

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -5,8 +5,14 @@ fun main() {
     val maxAverageWeight = 100
     val maxAverageVolume = 100
 
-    println("Average для груза с весом 20 кг и объемом 80 л: ${20 >= minAverageWeight && 20 <= maxAverageWeight && 80 < maxAverageVolume}")
+    val cargo1Weight = 20
+    val cargo1Volume = 80
 
-    println("Average для груза с весом 50 кг и объемом 100 л: ${50 >= minAverageWeight && 50 <= maxAverageWeight && 100 < maxAverageVolume}")
+    println("Average для груза с весом 20 кг и объемом 80 л: ${cargo1Weight >= minAverageWeight && cargo1Weight <= maxAverageWeight && cargo1Volume < maxAverageVolume}")
+
+    val cargo2Weight = 50
+    val cargo2Volume = 100
+
+    println("Average для груза с весом 50 кг и объемом 100 л: ${cargo2Weight >= minAverageWeight && cargo2Weight <= maxAverageWeight && cargo2Volume < maxAverageVolume}")
 
 }
