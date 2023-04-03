e2e50df4cc45d38d366d8bbf991caee4ccdfc03d
yuriy <yuriyyakushev@yandex.ru>
2023-04-03 22:10:57 +0300

    KS-2-1 rename

--- src/main/kotlin/lesson_2/lesson2_task_1.kt
+++
@@ -1,8 +0,0 @@
-package lesson_2
-
-import java.util.Locale
-
-fun main() {
-    val classGrades = arrayListOf(3, 4, 3, 5)
-    println(String.format(Locale.GERMANY, "%.2f", classGrades.average()))
-}
