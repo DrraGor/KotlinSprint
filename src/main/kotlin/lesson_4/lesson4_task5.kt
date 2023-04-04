f63fcdca7c9ffad2c1811c0777b07348cb8b7647
yuriy <yuriyyakushev@yandex.ru>
2023-04-04 20:10:11 +0300

    KS-4-5 Solving task number 5 with readln

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,25 +1,23 @@
 package lesson_4
 
-import java.util.Scanner
-
 const val RECOMMEND_TEAM_SIZE = 70
 const val MINIMUM_TEAM_SIZE = 55
 const val MINIMUM_PROVISION = 50
 
 fun main() {
-    val scanner = Scanner(System.`in`)
+
 
     println("Повреждён ли корпус корабля?")
-    val corpusCondition = scanner.next() == "нет"
+    val corpusCondition = readln() == "нет"
     println("Какова текущая численность экипажа?")
-    val teamSize = scanner.nextInt()
+    val teamSize = readln().toInt()
     println("Какова текущая численность ящиков провизии?")
-    val provision = scanner.nextInt()
+    val provision = readln().toInt()
     println("Благоприятные ли метеоусловия?")
-    val weather = scanner.next() == "да"
+    val weather = readln() == "да"
     val permission =
         ((!corpusCondition && weather && teamSize == RECOMMEND_TEAM_SIZE && provision > MINIMUM_PROVISION) ||
-          (corpusCondition && teamSize > MINIMUM_TEAM_SIZE && teamSize < RECOMMEND_TEAM_SIZE && provision > MINIMUM_PROVISION))
+                (corpusCondition && teamSize > MINIMUM_TEAM_SIZE && teamSize < RECOMMEND_TEAM_SIZE && provision > MINIMUM_PROVISION))
 
     println("Выход в море: $permission")
 
