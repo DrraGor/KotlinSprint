ac26fb804dc4a033f5a2a40249bfa865c5e241d1
yuriy <yuriyyakushev@yandex.ru>
2023-04-06 21:53:49 +0300

    KS-6-1 Solving task number 1

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,38 @@
+package lesson_6
+
+fun main() {
+    var successfulRegistration = false
+    println("Для регистрации введите логин:")
+
+    val userLogin = readln()
+
+    println("Введите пароль")
+
+    val userPassword = readln()
+
+    println("""
+        Поздравляем, регистрация прошла успешно
+        для входа введите логин
+        """.trimIndent())
+
+    if (readln() == userLogin) {
+        println("Введите пароль")
+
+        if (readln() == userPassword) {
+            println("Авторизация прошла успешно")
+            successfulRegistration = true
+
+        } else println("Неправильный пароль")
+
+        while (!successfulRegistration) {
+            println("Введите пароль")
+
+            if (readln() == userPassword) {
+                println("Авторизация прошла успешно")
+                successfulRegistration = true
+
+            } else println("Неправильный пароль")
+        }
+    } else println("Пользователь с таким логином не существует. Желаете зарегистритоваться?")
+
+}
