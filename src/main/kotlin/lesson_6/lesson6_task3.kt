38c4b465b3198138410dc8254ed10b3b22adfae3
yuriy <yuriyyakushev@yandex.ru>
2023-04-06 23:12:52 +0300

    KS-6-3 Solving task number 3

---
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -0,0 +1,13 @@
+package lesson_6
+
+fun main() {
+    println("Введите количество секунд которые нужно отсчитать:")
+    var countTime = readLine()!!.toInt()
+
+    while (countTime > 0) {
+        Thread.sleep(1000)
+        println("Осталось секунд: $countTime")
+        countTime--
+    }
+    println("Время вышло")
+}
