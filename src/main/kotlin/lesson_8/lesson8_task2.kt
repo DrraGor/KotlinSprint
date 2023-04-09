b4ee474fa91cdccfd50b566c2418fb05b80f4add
yuriy <yuriyyakushev@yandex.ru>
2023-04-09 16:42:36 +0300

    KS-8-2 Solving task number 2

---
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -0,0 +1,18 @@
+package lesson_8
+
+fun main() {
+    val arrayOfIngredients = arrayOf("Мука", "Соль", "Яйцо", "Сахар", "Дрожжи")
+    println("Какой ингредиент Вы хотите найти?")
+    val searchable = readln()
+    var successSearchFlag = false
+    for (i in arrayOfIngredients) {
+
+        if (i == searchable) {
+            println("Ингредиент $i в рецепте есть")
+            successSearchFlag = true
+            break
+        }
+
+    }
+    if (!successSearchFlag) println("Такого ингредиента в рецепте нет")
+}
