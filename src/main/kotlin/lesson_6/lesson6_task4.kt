acae151ee7e6331e191cbfb893ff8877d6844842
yuriy <yuriyyakushev@yandex.ru>
2023-04-09 15:05:00 +0300

    KS-6-4 Solving task number 4

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,15 +1,17 @@
 package lesson_6
 
+import kotlin.random.Random
+
 fun main() {
 
-    val secretNumber = 7
+    val secretNumber = Random.nextInt(1, 9)
     var attempts = 5
-    println("Угадайте число от 1 до 10:")
+    println("Угадайте число от 1 до 9:")
 
     while (attempts > 0) {
         if (secretNumber == readln().toInt()) {
             println("Это была великолепная игра!")
-        } else println("Не верно, попробуйте ещё раз")
+        } else println("Не верно")
         attempts--
     }
     println("Было загадано число $secretNumber")
