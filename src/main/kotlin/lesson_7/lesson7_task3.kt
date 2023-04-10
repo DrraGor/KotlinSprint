2cbaacc7c648013564529c3ef314da2366c15aa4
yuriy <yuriyyakushev@yandex.ru>
2023-04-10 21:19:31 +0300

    KS-7-3 Solving task number 3 with steps

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -3,11 +3,6 @@ package lesson_7
 fun main() {
 
     println("Введите число для начала отсчета")
-
     val borderOfCount = readLine()?.toInt() ?: 0
-    for (i in 0..borderOfCount) {
-        if (i % 2 == 0) println(i) else continue
-
-    }
-
+    for (i in 0..borderOfCount step 2) println(i)
 }
