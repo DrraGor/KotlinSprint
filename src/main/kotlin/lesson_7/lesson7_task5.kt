a9df62901db8bf2232d65bd03dfe781bf0f7cbdc
yuriy <yuriyyakushev@yandex.ru>
2023-04-10 21:27:53 +0300

    KS-7-5 Solving task number 5 with diapasons

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -2,14 +2,18 @@ package lesson_7
 
 fun main() {
 
-    val symbolsSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lettersSet = ('a'..'z')
+    val capitalLettersSet = ('A'..'Z')
+    val numbersSet = (0..9)
 
     println("Укажите длинну генерируемого пароля")
+
     val lengthGeneratedPass = readln().toInt()
+    val symbols = lettersSet + capitalLettersSet + numbersSet
 
     var generatedPassword = ""
     for (i in 1..lengthGeneratedPass) {
-        generatedPassword += symbolsSet.random()
+        generatedPassword += symbols.random()
 
     }
     println(generatedPassword)
