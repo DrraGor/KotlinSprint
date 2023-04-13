8e8c2bfb95a172be9ed8baa8776af1868e56cab9
yuriy <yuriyyakushev@yandex.ru>
2023-04-13 20:22:56 +0300

    KS-10-2 Solving task number 2

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,15 +1,13 @@
 package lesson_10
-
 fun main() {
 
     println("Введите логин")
     val userLogin = readln()
     println("Введите пароль")
     val userPassword = readln()
-
-    fun lengthChecker(incomeLogin: String, incomePassword: String) {
-        if (incomeLogin.length >= 4 && incomePassword.length >= 4) println("Регистрация прошла успешно")
-        else println("Логин или пароль недостаточно длинные")
-    }
     lengthChecker(userLogin, userPassword)
+}
+fun lengthChecker(incomeLogin: String, incomePassword: String) {
+    if (incomeLogin.length >= 4 && incomePassword.length >= 4) println("Регистрация прошла успешно")
+    else println("Логин или пароль недостаточно длинные")
 }
