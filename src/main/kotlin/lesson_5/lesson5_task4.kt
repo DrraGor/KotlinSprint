3a08d81e26a568d337c4544c4847460b97163933
yuriy <yuriyyakushev@yandex.ru>
2023-04-15 12:55:34 +0300

    KS-5-4 Solving task number 4

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -5,11 +5,10 @@ const val USER_PASSWORD_KS5_4 = "password"
 
 fun main() {
     println("Введите логин:")
-    val userLogin = readln()
-    if (userLogin == USER_LOGIN_KS5_4) {
-        println("Введите пароль")
-        val userPassword = readln()
-        if (userPassword == USER_PASSWORD_KS5_4) println("Добро пожаловать $USER_LOGIN_KS5_4") else println("Неправильный пароль")
+
+    if (readln() == USER_LOGIN_KS5_4) {
+        println("Введите пароль:")
+        if (readln() == USER_PASSWORD_KS5_4) println("Добро пожаловать $USER_LOGIN_KS5_4") else println("Неправильный пароль")
     } else println("Пользователь с таким логином не существует. Желаете зарегистритоваться?")
 
 }
