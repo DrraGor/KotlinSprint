bf189b4734e8c65fde6e555daf5b3735e947ca87
yuriy <yuriyyakushev@yandex.ru>
2023-04-15 16:20:13 +0300

    KS-9-5 Solving task number 5

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,14 +1,13 @@
 package lesson_9
 
 fun main() {
-    val arrayOfIngredients = mutableListOf<String>()
+    val arrayOfIngredients = mutableSetOf<String>()
 
     for (i in 1..5) {
         println("Введите название ингредиента № $i ")
         val incomeString = readln().lowercase()
-        if (!arrayOfIngredients.contains(incomeString)) {
-            arrayOfIngredients.add(incomeString)
-        } else continue
+        arrayOfIngredients.add(incomeString)
+
     }
     println(arrayOfIngredients.sorted().joinToString(", ", "", ".")
         .replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() })
