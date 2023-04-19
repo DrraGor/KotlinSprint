11923b74a52c92a765b93ac9b9fb74157124bce0
yuriy <yuriyyakushev@yandex.ru>
2023-04-19 23:11:36 +0300

    KS-5-5 Solving task number 5

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,39 +1,25 @@
 package lesson_5
 
-val FIRST_NUMBER_KS5 = (1..100).random()
-val SECOND_NUMBER_KS5 = (1..100).random()
-
 fun main() {
+    val firstNumber = (1..100).random()
+    val secondNumber = (1..100).random()
     println(
         """ 
-       Добро пожаловать в розыгрыш лотереи
+       Добро пожаловать в розыгрыш лотереи        
        что бы выиграть приз Вам нужно угадать два числа от 1 до 100!
        Введите первое число:
-    """.trimIndent()
-    )
+        """.trimIndent()
+        )
     val firstUserNumber = readln().toInt()
     println("Введите второе число:")
     val secondUserNumber = readln().toInt()
-    if ((firstUserNumber == FIRST_NUMBER_KS5 || firstUserNumber == SECOND_NUMBER_KS5) &&
-        (secondUserNumber == FIRST_NUMBER_KS5 || secondUserNumber == SECOND_NUMBER_KS5)
-    ) println(
-        """
-        Поздравляем! Вы выиграли главный приз!
-        Выигрышными номерами были $FIRST_NUMBER_KS5 и $SECOND_NUMBER_KS5
-        """.trimIndent()
-    )
-    else if ((firstUserNumber == FIRST_NUMBER_KS5 || firstUserNumber == SECOND_NUMBER_KS5) ||
-        (secondUserNumber == FIRST_NUMBER_KS5 || secondUserNumber == SECOND_NUMBER_KS5)
-    ) println(
-        """
-        Вы выиграли утешительный приз!
-        Выигрышными номерами были $FIRST_NUMBER_KS5 и $SECOND_NUMBER_KS5
-        """.trimIndent()
-    )
-    else println(
-        """
-        Неудача! Крутите барабан!
-        Выигрышными номерами были $FIRST_NUMBER_KS5 и $SECOND_NUMBER_KS5
-        """.trimIndent()
-    )
+    if ((firstUserNumber == firstNumber || firstUserNumber == secondNumber) &&
+        (secondUserNumber == firstNumber || secondUserNumber == secondNumber)
+    ) println("Поздравляем! Вы выиграли главный приз!")
+    else if ((firstUserNumber == firstNumber || firstUserNumber == secondNumber) ||
+        (secondUserNumber == firstNumber || secondUserNumber == secondNumber)
+    ) println("Вы выиграли утешительный приз!")
+    else println("Неудача! Крутите барабан!")
+    println("Выигрышными номерами были $firstNumber и $secondNumber")
+
 }
