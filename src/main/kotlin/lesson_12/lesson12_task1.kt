e8455b357efccbfee230ed4de5c3814fc64e5b82
yuriy <yuriyyakushev@yandex.ru>
2023-04-22 10:35:57 +0300

    KS-12-1 Solving task number 1

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -5,7 +5,7 @@ fun main() {
     val weatherTrackerOne = WeatherTrack()
     weatherTrackerOne.printInfo("weatherTrackerOne")
     weatherTrackerOne.dayTimeTemperature = 19
-    weatherTrackerOne.nightTimeTemperarure = 10
+    weatherTrackerOne.nightTimeTemperature = 10
     weatherTrackerOne.happenedRain = true
     weatherTrackerOne.atmosphericPressure = 781
     weatherTrackerOne.printInfo("weatherTrackerOne")
@@ -13,23 +13,22 @@ fun main() {
     val weatherTrackerTwo = WeatherTrack()
     weatherTrackerTwo.printInfo("weatherTrackerTwo")
     weatherTrackerTwo.dayTimeTemperature = 3
-    weatherTrackerTwo.nightTimeTemperarure = -5
+    weatherTrackerTwo.nightTimeTemperature = -5
     weatherTrackerTwo.happenedRain = false
     weatherTrackerTwo.atmosphericPressure = 750
     weatherTrackerTwo.printInfo("weatherTrackerTwo")
-
 }
 
-class  WeatherTrack (){
-    var dayTimeTemperature : Int = 23
-    var nightTimeTemperarure : Int = 15
-    var happenedRain : Boolean = false
-    var atmosphericPressure : Int = 769
+class  WeatherTrack {
+    var dayTimeTemperature = 23
+    var nightTimeTemperature = 15
+    var happenedRain = false
+    var atmosphericPressure = 769
 
     fun printInfo(name: String){
         println("""        
  Объект $name: Дневная температура - $dayTimeTemperature 
-                           Ночная температура - $nightTimeTemperarure
+                           Ночная температура - $nightTimeTemperature
                            Дождь - $happenedRain
                            Атмосферное давление - $atmosphericPressure        
         """)}
