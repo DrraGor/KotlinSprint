af00934f836eeecac4d5492ee023429fddacf67a
yuriy <yuriyyakushev@yandex.ru>
2023-04-22 11:50:41 +0300

    KS-11-5 Solving task number 5

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -2,55 +2,48 @@ package lesson_11
 
 fun main() {
     val forum = Forum()
-    val user1 = forum.newUser(1, "Роман Петров", "qweee", "romanoff@mail.ru")
-    val user2 = forum.newUser(1, "Логинова Ирина", "dlkjfsl", "log12@mail.ru", "Актриса оперы и балета")
+    val user1 = forum.newUser("Роман Петров", "qweee", "romanoff@mail.ru")
+    val user2 = forum.newUser("Логинова Ирина", "dlkjfsl", "log12@mail.ru", "Актриса оперы и балета")
+
     user1.printUserInfo()
     user2.printUserInfo()
-    forum.newMessage(1000, "Роман Петров", "Доброе утро")
-    forum.newMessage(1001, "Логинова Ирина", "Погода сегодня отличная")
-    forum.newMessage(1000, "Роман Петров", "Вечером обещают дождь")
-    forum.newMessage(1001, "Логинова Ирина", "Надеюсь прогноз не сбудется")
-
     println()
+    forum.newMessage(user1.getUserId(), user1.getUserLogin(), "Доброе утро")
+    forum.newMessage(user2.getUserId(), user2.getUserLogin(), "Погода сегодня отличная")
+    forum.newMessage(user1.getUserId(), user1.getUserLogin(), "Вечером обещают дождь")
+    forum.newMessage(user2.getUserId(), user2.getUserLogin(), "Надеюсь прогноз не сбудется")
+    println()
+    println("Хронология сообщений")
     forum.printThread()
 }
 
 interface UserInterface {
     fun printUserInfo()
+    fun getUserId(): Int
+    fun getUserLogin(): String
 }
 
 class Forum {
-    private var id = 10000
+    private var lastUserId = 0
     private var historyOfMessages = mutableListOf<String>()
-    private val factoryUser = User.Factory
-    private val factorySimpleUser = UserSimple.Factory
 
-    fun newUser(_id: Int, _login: String, _password: String, _mail: String, _bio: String): UserInterface {
-        val user = factoryUser.buildUser()
-        user.id = createUserId()
-        user.login = _login
-        user.password = _password
-        user.mail = _mail
-        user.bio = _bio
-        return user
+
+    fun newUser(_login: String, _password: String, _mail: String, _bio: String): UserInterface {
+        val userNew = User(_login, _password, _mail, _bio)
+        userNew.id = lastUserId
+        lastUserId++
+        return userNew
     }
 
-    fun newUser(_id: Int, _login: String, _password: String, _mail: String): UserInterface {
-        val user = factorySimpleUser.buildUser()
-        user.id = createUserId()
-        user.login = _login
-        user.password = _password
-        user.mail = _mail
-
-        return user
-    }
-
-    private fun createUserId(): Int {
-        return id++
+    fun newUser(_login: String, _password: String, _mail: String): UserInterface {
+        val userNew = UserSimple(_login, _password, _mail)
+        userNew.id = lastUserId
+        lastUserId++
+        return userNew
     }
 
     fun newMessage(authorId: Int, login: String, message: String) {
-        val newMessage = "$login: $message."
+        val newMessage = "$authorId $login: $message."
         historyOfMessages.add(newMessage)
         println(newMessage)
     }
@@ -61,39 +54,35 @@ class Forum {
 
 }
 
+class UserSimple(var login: String, var password: String, var mail: String) : UserInterface {
 
-abstract class UserFactory {
-    abstract fun buildUser(): UserInterface
-}
-
-
-class UserSimple(var id: Int, var login: String, var password: String, var mail: String) : UserInterface {
-
-    companion object Factory : UserFactory() {
-        override fun buildUser() = UserSimple(0, "Андреева Ольга", "fdfjh", "andreeva@mail.ru")
-    }
-
+    var id = 0
     override fun printUserInfo() {
         println("Id = $id, login = $login, password = $password, mail = $mail")
     }
+
+    override fun getUserId(): Int {
+        return id
+    }
+
+    override fun getUserLogin(): String {
+        return login
+    }
 }
 
-class User(var id: Int, var login: String, var password: String, var mail: String, var bio: String) :
+class User(var login: String, var password: String, var mail: String, var bio: String = "Без описания") :
     UserInterface {
-
-    companion object Factory : UserFactory() {
-
-        override fun buildUser() = User(0, "Пётр Сергеев", "qwerty", "sergeev@mail.ru", "")
-    }
-
-
+    var id = 0
     override fun printUserInfo() {
-        println("Id = $id, login = $login, password = $password, mail = $mail")
+        println("Id = $id, login = $login, password = $password, mail = $mail, bio = $bio")
     }
 
-    fun addBio() {
-        println("Введите описание пользователя")
-        bio = readln()
+    override fun getUserId(): Int {
+        return id
+    }
+
+    override fun getUserLogin(): String {
+        return login
     }
 
     fun passwordChanger() {
