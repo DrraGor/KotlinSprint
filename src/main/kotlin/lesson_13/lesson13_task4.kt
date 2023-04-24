00352eeb8b1043216e8df2b98c2db7001f1e578a
yuriy <yuriyyakushev@yandex.ru>
2023-04-24 21:53:09 +0300

    KS-13-4 Solving task number 4

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,47 @@
+package lesson_13
+
+class PhoneContactLesson4(private val _name: String?, private val _phone: Long?, private val _company: String?) {
+    fun printContact() {
+        println(
+            """
+            Имя: ${_name ?: "не указано"}
+            Номер: ${_phone ?: "не указано"}
+            Компания: ${_company ?: "не указано"}
+            
+            """.trimIndent()
+        )
+    }
+}
+
+val contactList = mutableListOf<PhoneContactLesson4>()
+fun main() {
+
+    addContact()
+    contactList.forEach { it.printContact() }
+}
+
+fun addContact() {
+    var nextContact = true
+    var phone: Long?
+    do {
+        println("Для добавления контакта введите его имя")
+        val name = readlnOrNull()
+        println("Введите номер телефона")
+
+        phone = try {
+            readLine()?.toLong()
+        } catch (ex: NumberFormatException) {
+            null
+        }
+
+        println("Введите название компании")
+        val company = readlnOrNull()
+
+        if (phone != null) contactList.add(PhoneContactLesson4(name, phone, company))
+        else continue
+        println("Если хотите добавить новую запись, введите \"да\"")
+
+        if (readLine() != "да") nextContact = false
+        else continue
+    } while (nextContact)
+}
