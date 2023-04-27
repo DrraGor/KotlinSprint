8892a6402dead9d1e9bc6cdf0d5297a5bf4cc70e
yuriy <yuriyyakushev@yandex.ru>
2023-04-27 21:16:41 +0300

    KS-13-5 Solving task number 5

---
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -0,0 +1,11 @@
+package lesson_13
+
+fun main() {
+    val number: Long
+    val strNumber = readLine().toString()
+    try {
+        number = strNumber.toLong()!!
+    } catch (_: NumberFormatException) {
+        println("Номер может содержать только цифры")
+    }
+}
