ac88e4ee8798d896c4aabfd9b7551f984ad6e89a
yuriy <yuriyyakushev@yandex.ru>
2023-04-30 15:11:11 +0300

    KS-17-4 Solving task number 4

---
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -0,0 +1,26 @@
+package lesson_17
+
+
+fun main() {
+    val parcel = Parcel(1, "Vitebsk")
+    parcel.location = "Moskva"
+    parcel.location = "Vologda"
+    parcel.location = "Omsk"
+}
+
+class Parcel(val number: Int, _location: String) {
+
+    private var forwarding = 0
+        set(value) {
+             field = value
+            println("Счетчик: $field")
+        }
+    var location = _location
+        set(value) {
+            if (value != field) {
+                forwarding += 1
+            }
+            field = value
+        }
+
+}
