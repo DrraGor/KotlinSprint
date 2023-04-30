78f3bb2760729d72f53285c1b73463736760bdfe
yuriy <yuriyyakushev@yandex.ru>
2023-04-30 11:34:51 +0300

    KS-16-2 Solving task number 2

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -2,19 +2,20 @@ package lesson_16
 
 fun main() {
     val circle = Circle(22)
-    println(circle.perimeter())
-    println(circle.square())
+    println(circle.calculateThePerimeter())
+    println(circle.calculateTheSquare())
 }
 
 private class Circle(private val radius: Int) {
 
     private val pi = 3.14
 
-    fun square(): Double {
+    fun calculateTheSquare(): Double {
         return pi * (radius * radius).toDouble()
     }
 
-    fun perimeter(): Double {
+
+    fun calculateThePerimeter(): Double {
         return 2 * pi * radius.toDouble()
     }
 
