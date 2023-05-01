c155b13b04418fa6c9a6049136118edf5e5ff0c3
yuriy <yuriyyakushev@yandex.ru>
2023-05-01 19:53:52 +0300

    KS-17-2 Solving task number 2

--- src/main/kotlin/lesson_17/lesson17_task2.kt
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -3,14 +3,10 @@ package lesson_17
 fun main() {
     val ship = Ship("Varyag", 18, "Vladivostok")
     ship.name = "SPB"
-
 }
 
-class Ship(_name: String, var speed: Int, var homePort: String){
+class Ship(_name: String, var speed: Int, var homePort: String) {
 
     var name: String = _name
-        set(value) {
-            println("Имя корабля изменить нельзя")
-
-        }
+        set(value) = println("Имя корабля изменить нельзя")
 }
