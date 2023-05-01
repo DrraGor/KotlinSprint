b143f2edf15b989b0b5c356633d5964d5ca1ac27
yuriy <yuriyyakushev@yandex.ru>
2023-05-01 20:33:30 +0300

    KS-17-4 Solving task number 4

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -5,20 +5,31 @@ fun main() {
     val parcel = Parcel(1, "Vitebsk")
     parcel.location = "Moskva"
     parcel.location = "Vologda"
+    println(parcel.location)
     parcel.location = "Omsk"
+    println(parcel.location)
+    parcel.location = "Omsk"
+    println(parcel.location)
+    parcel.location = "Vologda"
+    println(parcel.location)
+    parcel.location = "Vologda"
+    println(parcel.location)
 }
 
 class Parcel(val number: Int, _location: String) {
 
     private var forwarding = 0
         set(value) {
-             field = value
+            field = value
             println("Счетчик: $field")
         }
     var location = _location
         set(value) {
-            if (value != field) forwarding++
-            field = value
+            if (value != field) {
+                field = value
+                forwarding++
+            } else field
+
         }
 
 }
