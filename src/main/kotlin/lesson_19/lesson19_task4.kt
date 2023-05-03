5fd4733b407d5ab893f1af311047b4a3cc011655
yuriy <yuriyyakushev@yandex.ru>
2023-05-03 21:58:38 +0300

    KS-19-4 Solving task number 4

---
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -0,0 +1,43 @@
+package lesson_19
+
+fun main() {
+    val tank = Tank()
+    tank.fire()
+    tank.loadTheGun(Ammunition.BLUE)
+    tank.fire()
+    tank.loadTheGun(Ammunition.GREEN)
+    tank.fire()
+    tank.loadTheGun(Ammunition.RED)
+    tank.fire()
+
+}
+
+
+enum class Ammunition() {
+    RED,
+    BLUE,
+    GREEN,
+}
+
+fun defineAmmunitionDamage(ammunition: Ammunition): Int {
+    return when (ammunition) {
+        Ammunition.RED -> 20
+        Ammunition.GREEN -> 10
+        Ammunition.BLUE -> 5
+    }
+}
+
+class Tank() {
+    var currentAmmunition = 0
+
+    fun loadTheGun(ammunition: Ammunition) {
+        currentAmmunition = defineAmmunitionDamage(ammunition)
+        println("Орудие заряжено боеприпасом \"${ammunition.name}\"")
+    }
+
+    fun fire() {
+        if (currentAmmunition > 0) println("Бдыщь!!!! Нанесено $currentAmmunition единиц повреждений")
+        else println("Орудие не заряжено")
+    }
+
+}
