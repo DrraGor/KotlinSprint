8c7e4593900a604a7b1d5ee5ce311b8fc9c97b7d
yuriy <yuriyyakushev@yandex.ru>
2023-05-06 00:11:32 +0300

    KS-20-1 Solving task number 1

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -1,8 +1,8 @@
 package lesson_20
 
-fun main(){
- val username = "Аркадий"
-    {userName: String  ->  println("С наступающим Новым Годом, $userName")}(username)
+fun main() {
+    val username = "Аркадий"
+    { userName: String -> println("С наступающим Новым Годом, $userName") }(username)
 
 }
 
