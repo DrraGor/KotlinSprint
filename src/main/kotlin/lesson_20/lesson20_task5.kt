84d1e600fc8580847172bfd2004afb8c433dd47e
Юрий <93224591+DrraGor@users.noreply.github.com>
2023-05-13 14:40:43 +0300

    KS-20-5 Solving task number 5

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -2,26 +2,30 @@ package lesson_20
 
 fun main() {
     val robot = RobotLesson20()
-    println(robot.say())
-    robot.setModifier { println(robot.say().reversed()) }
+    robot.say()
+    robot.setModifier { robot.phrasesList.random().reversed() }
+    robot.say()
+    robot.setModifier { robot.phrasesList.random() }
+    robot.say()
 }
 
 class RobotLesson20 {
 
-    private val phrasesList = listOf(
+    val phrasesList = listOf(
         "Приветствую тебя человек, я робот помощник",
         "Что привело тебя в эти развалины",
         "Не мог бы ты поискать для меня аккумулятор",
         "Мой речевой модуль поврежден",
         "Вызовите технического специалиста для ремонта"
     )
+    var lambdaForSay = { phrasesList.random() }
+    fun say() {
+        println(lambdaForSay())
 
-    fun say(): String {
-        return phrasesList.random()
     }
 
-    fun setModifier(lambda: () -> Unit) {
-        lambda()
+    fun setModifier(lambda: () -> String) {
+        lambdaForSay = lambda
     }
 }
 
