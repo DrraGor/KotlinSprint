9d35d33d3741948ae17da5905f6f8cca1c6ebfc1
Юрий <93224591+DrraGor@users.noreply.github.com>
2023-05-16 22:44:13 +0300

    KS-20-5  task number 5

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -3,29 +3,37 @@ package lesson_20
 fun main() {
     val robot = RobotLesson20()
     robot.say()
-    robot.setModifier { robot.phrasesList.random().reversed() }
+    robot.setModifier { it: String -> it.reversed() }
     robot.say()
-    robot.setModifier { robot.phrasesList.random() }
     robot.say()
 }
 
 class RobotLesson20 {
 
-    val phrasesList = listOf(
+    private val phrasesList = listOf(
         "Приветствую тебя человек, я робот помощник",
         "Что привело тебя в эти развалины",
         "Не мог бы ты поискать для меня аккумулятор",
         "Мой речевой модуль поврежден",
         "Вызовите технического специалиста для ремонта"
     )
-    var lambdaForSay = { phrasesList.random() }
-    fun say() {
-        println(lambdaForSay())
+    private var mod = false                              // флаг модификатора
 
+    var lamBda = {s: String -> String}    // попытка создать переменную в которую потом запишется лямбда из setModifier (хрень)
+        fun say() {                                      // Говорить должны через say т.к. у нас два режима пишу через if/else
+
+        if (!mod) println( phrasesList.random())         // режим по умолчанию
+        else lamBda(phrasesList.random())                // режим с включенным модификатором
     }
 
-    fun setModifier(lambda: () -> String) {
-        lambdaForSay = lambda
+
+    fun setModifier(lambda: (String) -> String) {       // должны принять лямбду
+        if (!mod) {                                     // модификатор не разовая штука поэтому ставлю флаг его включения
+            mod = true
+            println()
+        } else mod = false
+         lambda(phrasesList.random())                   //проверка лямбды, принимает стринг и его корректно обрабатывает
+                                                        //как это транслировать в say  я понять не могу!!! HELP!!!
     }
+
 }
-
