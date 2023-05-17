709d82eeff922cf07053cc34c539661268c1fc63
Юрий <93224591+DrraGor@users.noreply.github.com>
2023-05-17 22:13:29 +0300

    KS-20-5 Solving task number 5

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -1,10 +1,15 @@
 package lesson_20
 
+import java.util.*
+
 fun main() {
     val robot = RobotLesson20()
     robot.say()
-    robot.setModifier { it: String -> it.reversed() }
+    robot.setModifier { it.reversed() }
     robot.say()
+    robot.setModifier { it.uppercase(Locale.getDefault()) }
+    robot.say()
+    robot.setModifier { it }
     robot.say()
 }
 
@@ -17,23 +22,17 @@ class RobotLesson20 {
         "Мой речевой модуль поврежден",
         "Вызовите технического специалиста для ремонта"
     )
-    private var mod = false                              // флаг модификатора
 
-    var lamBda = {s: String -> String}    // попытка создать переменную в которую потом запишется лямбда из setModifier (хрень)
-        fun say() {                                      // Говорить должны через say т.к. у нас два режима пишу через if/else
+    var lambdaForSayModifier = { text: String -> text }
 
-        if (!mod) println( phrasesList.random())         // режим по умолчанию
-        else lamBda(phrasesList.random())                // режим с включенным модификатором
+    fun say() {
+
+        println(lambdaForSayModifier(phrasesList.random()))
     }
 
+    fun setModifier(lambda: (String) -> String) {
+        lambdaForSayModifier = lambda
 
-    fun setModifier(lambda: (String) -> String) {       // должны принять лямбду
-        if (!mod) {                                     // модификатор не разовая штука поэтому ставлю флаг его включения
-            mod = true
-            println()
-        } else mod = false
-         lambda(phrasesList.random())                   //проверка лямбды, принимает стринг и его корректно обрабатывает
-                                                        //как это транслировать в say  я понять не могу!!! HELP!!!
     }
 
 }
